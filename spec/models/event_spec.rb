5ea77a67e11200426fa58f68049d2ec62de94eb1
treby <treby@atelier-nodoka.net>
2015-05-12 12:52:35 +0900

    Add test for Event.at

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -23,4 +23,24 @@ describe Event do
       specify { expect(build(:event, started_at: '2015-01-29 17:00:00 +0900', ended_at: '2015-02-08 23:59:59 +0900')).to be_valid }
     end
   end
+
+  describe 'find by the time' do
+    before do
+      @event = create(:event, name: 'Event name', started_at: '2015-01-16 17:00:00 +0900', ended_at: '2015-01-27 23:59:59 +0900')
+    end
+
+    context 'not specify target time' do
+      subject { Event.at }
+      around(:example) { |ex| Timecop.freeze(now) { ex.run } }
+
+      context 'when there is no event for now' do
+        let(:now) { '2015-12-10 10:00:00 +0900' }
+        it { should be_nil }
+      end
+      context 'when now is event term' do
+        let(:now) { '2015-01-16 17:00:00 +0900' }
+        it { should eq @event }
+      end
+    end
+  end
 end
