19d045235f629f640cefef4f6acb577daf5f0353
treby <treby@atelier-nodoka.net>
2015-05-12 11:31:36 +0900

    Implement Event model.

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,8 +1,7 @@
 FactoryGirl.define do
   factory :event do
-    name "MyString"
-started_at "2015-05-12 11:19:01"
-ended_at "2015-05-12 11:19:01"
+    name 'Event name'
+    sequence(:started_at) { |n| Time.parse('2015-01-16 17:00:00 +0900') + ((n - 1) * 12).days }
+    sequence(:ended_at) { |n| Time.parse('2015-01-27 23:59:59 +0900') + ((n - 1) * 12).days }
   end
-
 end
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Event do
+  describe 'validation' do
+    specify { expect(build(:event)).to be_valid }
+    specify { expect(build(:event, name: '')).not_to be_valid }
+    specify { expect(build(:event, started_at: nil)).not_to be_valid }
+    specify { expect(build(:event, ended_at: nil)).not_to be_valid }
+    specify { expect(build(:event, started_at: '2015-01-07 17:00:00 +0900', ended_at: '2015-01-07 18:00:00 +0900')).to be_valid }
+    specify { expect(build(:event, started_at: '2015-01-07 17:00:00 +0900', ended_at: '2015-01-07 17:00:00 +0900')).not_to be_valid }
+    specify { expect(build(:event, started_at: '2015-01-07 18:00:00 +0900', ended_at: '2015-01-07 17:00:00 +0900')).not_to be_valid }
+
+    describe 'term must not cover other one.' do
+      before do
+        @event = create(:event, name: 'Event name', started_at: '2015-01-16 17:00:00 +0900', ended_at: '2015-01-27 23:59:59 +0900')
+      end
+      specify { expect(@event).to be_valid }
+      specify { expect(build(:event, started_at: '2015-01-07 17:00:00 +0900', ended_at: '2015-01-14 23:59:59 +0900')).to be_valid }
+      specify { expect(build(:event, started_at: '2015-01-07 17:00:00 +0900', ended_at: '2015-02-08 23:59:59 +0900')).not_to be_valid }
+      specify { expect(build(:event, started_at: '2015-01-07 17:00:00 +0900', ended_at: '2015-01-17 00:00:00 +0900')).not_to be_valid }
+      specify { expect(build(:event, started_at: '2015-01-17 00:00:00 +0900', ended_at: '2015-01-25 00:00:00 +0900')).not_to be_valid }
+      specify { expect(build(:event, started_at: '2015-01-25 18:00:00 +0900', ended_at: '2015-02-08 23:59:59 +0900')).not_to be_valid }
+      specify { expect(build(:event, started_at: '2015-01-29 17:00:00 +0900', ended_at: '2015-02-08 23:59:59 +0900')).to be_valid }
+    end
+  end
 end
