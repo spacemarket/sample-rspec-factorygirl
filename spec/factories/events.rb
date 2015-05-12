60b393355fcab164407db0384e0bdfdd72e1f31d
treby <treby@atelier-nodoka.net>
2015-05-12 12:36:23 +0900

    Fix Event factory.

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -2,6 +2,6 @@ FactoryGirl.define do
   factory :event do
     name 'Event name'
     sequence(:started_at) { |n| Time.parse('2015-01-16 17:00:00 +0900') + ((n - 1) * 12).days }
-    sequence(:ended_at) { |n| Time.parse('2015-01-27 23:59:59 +0900') + ((n - 1) * 12).days }
+    ended_at { started_at.nil? ? Time.now : ( started_at + 1.day ) }
   end
 end
